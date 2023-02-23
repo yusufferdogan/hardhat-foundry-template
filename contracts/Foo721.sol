32b4fd88418f3f7b7d5d0fe606460e3016f85148
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2023-02-23 12:26:32 +0300

    rename src to contracts

---
+++ contracts/Foo721.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "forge-std/console.sol";
+
+contract Foo721 is ERC721, Ownable {
+    uint256 public tokenCount;
+
+    uint256 public constant MAX_SUPPLY = 100;
+    uint256 public constant FREE_MINT_COUNT = 5;
+    uint256 public constant MINT_PRICE = 0.001 ether;
+    // solhint-disable-next-line var-name-mixedcase
+    uint256 public immutable MINT_DATE;
+
+    mapping(address => bool) public freeMinted;
+
+    string public baseURI;
+
+    error InsufficientFunds();
+    error MaxSupplyExceeded();
+    error AlreadyFreeMinted();
+    error FreeMintExceeded();
+    error TransferTxError();
+    error InvalidDate();
+
+    constructor(uint256 _mintDate) ERC721("FooNFT", "F_NFT") {
+        MINT_DATE = _mintDate;
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        console.log(msg.sender);
+        baseURI = baseURI_;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function mint(address to) external payable {
+        // solhint-disable-next-line not-rely-on-time
+        if (block.timestamp < MINT_DATE) revert InvalidDate();
+
+        if (tokenCount + 1 > MAX_SUPPLY) revert MaxSupplyExceeded();
+
+        if (msg.value < MINT_PRICE) revert InsufficientFunds();
+
+        tokenCount++;
+
+        _safeMint(to, tokenCount);
+    }
+
+    function withdraw() external onlyOwner {
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool isSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+
+        if (!isSuccess) revert TransferTxError();
+    }
+}
